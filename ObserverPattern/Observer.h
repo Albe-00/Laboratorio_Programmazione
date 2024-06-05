15721f3f9a27def019fe5997edc4fff8304c4374
Albe-00 <120462716+Albe-00@users.noreply.github.com>
2024-06-05 11:28:12 +0200

    Observer Pattern : Definizione classi astratte Observer e Subject

--- ObserverPattern/Observer.h
+++ ObserverPattern/Observer.h
@@ -5,4 +5,16 @@
 #ifndef LABORATORIO_PROGRAMMAZIONE_OBSERVER_H
 #define LABORATORIO_PROGRAMMAZIONE_OBSERVER_H
 
+#include <string>
+
+// OBSERVER INTERFACE
+
+class Observer {
+public:
+    virtual ~Observer() {}
+
+    virtual void update(const std::string nomeLista , int oggettiMancanti) =0;
+
+};
+
 #endif //LABORATORIO_PROGRAMMAZIONE_OBSERVER_H
--- ObserverPattern/Subject.h
+++ ObserverPattern/Subject.h
@@ -5,4 +5,21 @@
 #ifndef LABORATORIO_PROGRAMMAZIONE_SUBJECT_H
 #define LABORATORIO_PROGRAMMAZIONE_SUBJECT_H
 
+#include "Observer.h"
+
+//SUBJECT INTERFACE
+
+class Subject {
+public:
+
+    virtual void subscribe(Observer* o)=0;
+    virtual void unsubscribe(Observer* o)=0;
+
+    virtual void notify()=0;
+
+    virtual ~Subject() = default;
+};
+
 #endif //LABORATORIO_PROGRAMMAZIONE_SUBJECT_H
+
+
